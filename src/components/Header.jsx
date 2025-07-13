974c66343ddc54fbd7a5ab327741b9b0649f4d04
hannalee7222 <hannah.lee7222@gmail.com>
2025-07-13 15:33:57 +0900

    12일차 과제

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 export default function Header() {
   return (
-    <header className="header-container">
-      <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+    <header className="flex justify-between items-center px-5 py-2 bg-black">
+      <h2 className="text-white text-lg font-semibold">OZ코딩스쿨</h2>
+      <ul className="flex items-center justify-center gap-5">
+        <li className="list-none text-white text-[13px] font-normal">로그인</li>
+        <li className="list-none text-white text-[13px] font-normal">
+          회원가입
+        </li>
+        <li className="list-none text-white text-[13px] font-normal">
+          내클래스
+        </li>
       </ul>
     </header>
   );
